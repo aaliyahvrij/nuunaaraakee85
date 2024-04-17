6f0cd13f915f4da7ebf926a8268748b6e0f97790
Salina Muhammad <salina.muhammad@hva.nl>
2024-04-17 16:25:03 +0200

    added lit

---
+++ src/web/src/components/GameObjectForm.ts
@@ -0,0 +1,12 @@
+import { html, LitElement, TemplateResult } from "lit";
+import { customElement  } from "lit/decorators.js";
+
+@customElement("gameobject-form")
+export class GameObjectForm extends LitElement {
+
+    public render(): TemplateResult {
+        return html`
+            <p>Hello world!</p>
+        `;
+    }
+}
--- src/web/src/index.ts
+++ src/web/src/index.ts
@@ -1,1 +1,2 @@
 console.log("Sprint 0");
+export * from "./components/GameObjectForm";
