ef9f727a7efef13766390abe0b5348c5e6683e0c
Stephanie Appiah Opoku <stephanie.appiah.opoku@hva.nl>
2024-04-24 14:17:10 +0200

    added express

---
+++ src/api/src/routes.ts
@@ -0,0 +1,15 @@
+import { Router } from "express";
+
+export const router: Router = Router();
+
+router.get("/", (_req, res) => {
+    res.json({
+        hello: "world",
+    });
+});
+
+
+router.post("/gameobject/add", (req, res) => {
+    console.log(req.body); 
+    res.status(204).send(); 
+});
