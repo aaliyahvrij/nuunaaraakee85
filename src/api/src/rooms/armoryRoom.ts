7c6264123a5d1e0155b263f380df2f2ecddab1a1
Stephanie Appiah Opoku <stephanie.appiah.opoku@hva.nl>
2024-05-08 21:45:09 +0200

    testing the weapon

--- src/api/src/rooms/armoryRoom.ts
+++ src/api/src/rooms/armoryRoom.ts
@@ -1,6 +1,7 @@
 import { ActionResult } from "../base/actionResults/ActionResult";
 import { TextActionResult } from "../base/actionResults/TextActionResult";
 import { Action } from "../base/actions/Action";
+import { CustomAction } from "../base/actions/CustomAction";
 import { ExamineAction } from "../base/actions/ExamineAction";
 import { GameObject } from "../base/gameObjects/GameObject";
 import { Room } from "../base/gameObjects/Room";
@@ -18,14 +19,16 @@ export class armoryRoom extends Room {
 
     public images(): string[] {
         return [
-            
+            // hier komt de foto
         ];
         
     }
 
     public actions(): Action[] {
-        return [new ExamineAction()];
+        return [new ExamineAction(), new CustomAction("test-me", "test the weapon", false)];
     }
+
+
 
     public objects(): GameObject[] {
         return [this];
@@ -35,6 +38,17 @@ export class armoryRoom extends Room {
     public examine(): ActionResult | undefined {
         return new TextActionResult(["Welcome to the Armory!", "Here, you'll forge the weapons of your destiny."]);
     }
+
+    public custom(alias: string, _gameObjects: GameObject[] | undefined): ActionResult | undefined {
+        if (alias === "test-me")
+
+            return new TextActionResult([" You test me for a weapon?"]);
+
+            return undefined;
+
+    }
+
+
     
 
 
