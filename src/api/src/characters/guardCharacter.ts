5fae8742c38132660c5680a1f0d3577ee13fde29
Stephanie Appiah Opoku <stephanie.appiah.opoku@hva.nl>
2024-05-17 23:31:04 +0200

    added text for character

--- src/api/src/characters/guardCharacter.ts
+++ src/api/src/characters/guardCharacter.ts
@@ -28,15 +28,29 @@ export class guardCharacter extends Character implements Examine {
         if(choiceId === 1) {
             return new TextActionResult(["The shield has a specific emblem that matches one of the weapons. Look for the weapon with a similar emblem."]);
         }
+
+        if(choiceId === 2){
+            return new TextActionResult(["The right weapon is both light and deadly. Choose wisely"]);
+        }
+
+        if(choiceId === 3){
+            return new TextActionResult(["This armory has stood for centuries, protecting our land with its finest weapons"]);
+        }
+
+        
+
+
         
         return new TalkActionResult(this, ["The shield in the center of the room holds the key to choosing the correct weapon. Remember, the right weapon is both light and deadly. If you choose the wrong weapon, you will need to try again."], [
-            new TalkChoiceAction(1, "Ask for more details about the shield.")
+            new TalkChoiceAction(1, "Ask for more details about the shield."), new TalkChoiceAction(2, "Ask for hints about the weapons."), new TalkChoiceAction(3,"Ask about the history of the armory.")
         ]);
 
       
         
         
     }
+
+    
  
 
 }
