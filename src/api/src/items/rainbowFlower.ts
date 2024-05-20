0a22cae6ad45cab9553c5902e328ccb14be9e356
Aaliyah <aaliyah.vrij@hva.nl>
2024-05-20 22:11:42 +0200

    ADD: rainbow flower

---
+++ src/api/src/items/rainbowFlower.ts
@@ -0,0 +1,27 @@
+import { Pickup, PickupActionAlias } from "../actions/PickupAction";
+import { ActionResult } from "../base/actionResults/ActionResult";
+import { TextActionResult } from "../base/actionResults/TextActionResult";
+import { Examine, ExamineActionAlias } from "../base/actions/ExamineAction";
+import { Item } from "../base/gameObjects/Item";
+
+export const rainbowFlowerAlias: string = "rainbow-flower";
+
+export class rainbowFlowerItem extends Item implements Examine, Pickup {
+    public constructor() {
+        super(rainbowFlowerAlias, ExamineActionAlias, PickupActionAlias);
+    }
+
+    public name(): string {
+        return "rainbow flower";
+    }
+
+    public examine(): ActionResult | undefined {
+        return new TextActionResult([
+            "This vibrant and colorful flower appears to have petals infused with different colors, resulting in a strikingly beautiful and mesmerizing bloom.",
+        ]);
+    }
+
+    public pickup(): ActionResult | undefined {
+        return new TextActionResult(["you pick up the colourful flower"]);
+    }
+}
