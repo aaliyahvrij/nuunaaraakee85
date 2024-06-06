68e64215d66d18029304d273d0468a64f35db5dd
Salina Muhammad <salina.muhammad@hva.nl>
2024-06-07 01:18:22 +0200

    fixed startroom

--- src/api/src/Salina/rooms/StartupRoom.ts
+++ src/api/src/Salina/rooms/StartupRoom.ts
@@ -6,6 +6,7 @@ import { GameObject } from "../../base/gameObjects/GameObject";
 import { Room } from "../../base/gameObjects/Room";
 import { getPlayerSession } from "../../instances";
 import { ExampleRoom } from "./ExampleRoom";
+import { LibraryRoom } from "./LibraryRoom";
 
 export const StartupRoomAlias: string = "startup";
 
@@ -32,7 +33,7 @@ export class StartupRoom extends Room {
 
     public custom(alias: string, _gameObjects?: GameObject[]): ActionResult | undefined {
         if (alias === "start-game") {
-            const room: ExampleRoom = new ExampleRoom();
+            const room: LibraryRoom = new LibraryRoom();
 
             //Set the current room to the example room
             getPlayerSession().currentRoom = room.alias;
