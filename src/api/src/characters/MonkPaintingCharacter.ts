03abd78598229123cb7457b7929f2e315c3bfcee
Aaliyah <aaliyah.vrij@hva.nl>
2024-06-11 19:52:39 +0200

    ADD: more painting characters

---
+++ src/api/src/characters/MonkPaintingCharacter.ts
@@ -0,0 +1,43 @@
+import { ActionResult } from "../base/actionResults/ActionResult";
+import { TalkActionResult } from "../base/actionResults/TalkActionResult";
+import { TextActionResult } from "../base/actionResults/TextActionResult";
+import { Examine, ExamineActionAlias } from "../base/actions/ExamineAction";
+import { TalkChoiceAction } from "../base/actions/TalkAction";
+import { Character } from "../base/gameObjects/Character";
+import { getPlayerSession } from "../instances";
+import { PlayerSession } from "../types";
+
+export const monkPaintingCharacterAlias: string = "monk-character";
+
+export class MonkPaintingCharacter extends Character implements Examine {
+    public constructor() {
+        super(monkPaintingCharacterAlias, ExamineActionAlias);
+    }
+
+    public name(): string {
+        return "Monk Painting";
+    }
+
+    public examine(): ActionResult | undefined {
+        return new TextActionResult([
+            "The paintings description has written:",
+            "A monk sitting cross-legged in deep meditation within a tranquil temple.",
+        ]);
+    }
+
+    public talk(choiceId?: number | undefined): ActionResult | undefined {
+        const playerSession: PlayerSession = getPlayerSession();
+
+        playerSession.hasTalkedToMonk = true;
+
+        if (choiceId === 1) {
+            return new TextActionResult([
+                "In silence, echoes reveal what is hidden. Seek the place where sounds converge.",
+            ]);
+        }
+
+        const choiceActions: TalkChoiceAction[] = [new TalkChoiceAction(1, "Listen carefully...")];
+
+        return new TalkActionResult(this, ["*voices..*"], choiceActions);
+    }
+}
---
+++ src/api/src/characters/StonePaintingCharacter.ts
@@ -0,0 +1,43 @@
+import { ActionResult } from "../base/actionResults/ActionResult";
+import { TalkActionResult } from "../base/actionResults/TalkActionResult";
+import { TextActionResult } from "../base/actionResults/TextActionResult";
+import { Examine, ExamineActionAlias } from "../base/actions/ExamineAction";
+import { TalkChoiceAction } from "../base/actions/TalkAction";
+import { Character } from "../base/gameObjects/Character";
+import { getPlayerSession } from "../instances";
+import { PlayerSession } from "../types";
+
+export const stonePaintingCharacterAlias: string = "stone-character";
+
+export class StonePaintingCharacter extends Character implements Examine {
+    public constructor() {
+        super(stonePaintingCharacterAlias, ExamineActionAlias);
+    }
+
+    public name(): string {
+        return "Stone Painting";
+    }
+
+    public examine(): ActionResult | undefined {
+        return new TextActionResult([
+            "The paintings description has written:",
+            "A radiant stone glowing softly, set within an ornate frame. The stone seems to emit a gentle light that illuminates its surroundings.",
+        ]);
+    }
+
+    public talk(choiceId?: number | undefined): ActionResult | undefined {
+        const playerSession: PlayerSession = getPlayerSession();
+
+        playerSession.hasTalkedtoStone = true;
+
+        if (choiceId === 1) {
+            return new TextActionResult([
+                "rests in silence, concealed within the walls. Let the sounds guide you",
+            ]);
+        }
+
+        const choiceActions: TalkChoiceAction[] = [new TalkChoiceAction(1, "Listen carefully...")];
+
+        return new TalkActionResult(this, ["*voices..*"], choiceActions);
+    }
+}
